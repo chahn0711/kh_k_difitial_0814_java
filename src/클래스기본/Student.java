6e06bdea6b9ac0d02a9943696bb1ff7b9ea5d64c
chahn0711 <chahn0711@naver.com>
2023-08-22 12:48:12 +0900

    20230822, 오전 수업끝2

--- src/클래스기본/Student.java
+++ src/클래스기본/Student.java
@@ -1,22 +1,22 @@
 package 클래스기본;
 
 public class Student {
-    // private 외부에서 해당 필드에 대한 접근 막음
+    // private 외부에서 해당 필드에 대한 접근을 막음
     private int id; // 학번, 필드라고 부름
     private String name; // 이름
     private int grade; // 학년
     private String addr; // 주소
-
     // 메소드
-    public void showInfo() {
+    public  void showInfo() {
         System.out.println(name + "," + addr);
     }
-
-    public void setName(String name) {
+    public String setName(String name) {
         this.name = name;
+        return  "참입니다.";
     }
 
     public void setAddr(String addr) {
         this.addr = addr;
     }
 }
+
