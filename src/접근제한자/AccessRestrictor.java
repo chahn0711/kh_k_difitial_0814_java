77c43fa41e8029844d550512e8f595fed95d3943
chahn0711 <chahn0711@naver.com>
2023-08-25 08:58:36 +0900

    20230824 오후수업

---
+++ src/접근제한자/AccessRestrictor.java
@@ -0,0 +1,29 @@
+package 접근제한자;
+
+import 인스턴스필드.Parent;
+
+public class AccessRestrictor {
+    public static void main(String[] args) {
+      Child child = new Child("엔지니어");
+        System.out.println(child.getName());
+//        System.out.println(child.getMoney());
+        System.out.println(child.getAddr());
+    }
+}
+
+class Child extends Parent {
+    String jobs;
+    public Child(String jobs) {
+        this.jobs = jobs;
+    }
+//    public String getMoney() {
+//        return money;
+//    }
+    public String getName() {
+        return name;
+    }
+    public String getAddr() {
+        return addr;
+    }
+}
+
