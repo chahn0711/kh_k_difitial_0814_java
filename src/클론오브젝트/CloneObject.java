cfbd930552ef9c18a5fae97b7507870a57f6acc8
chahn0711 <chahn0711@naver.com>
2023-08-29 14:49:52 +0900

    20230829 오후 수업 오브젝트 클래스

--- src/클론오브젝트/CloneObject.java
+++ src/클론오브젝트/CloneObject.java
@@ -1,9 +1,9 @@
 package 클론오브젝트;
 
-public class CloneObject {
+public class CloneObject implements Cloneable {
     public static void main(String[] args) {
         Member mem1 = new Member("bear", "곰돌이사육사", "123456", 20, true);
-        Member mem2 = mem1;
+        Member mem2 = mem1.getMember(); // 얕은 복사가 일어나는 조건
         mem1.viewInfo();
         mem2.name = "KH정보교육원";
         mem2.password = "KKKKKK";
