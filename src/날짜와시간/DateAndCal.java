6deb9d56a94fbb79c8002e131d9bdc56452aa218
chahn0711 <chahn0711@naver.com>
2023-08-29 17:32:55 +0900

    20230829 오후 수업

--- src/날짜와시간/DateAndCal.java
+++ src/날짜와시간/DateAndCal.java
@@ -1,6 +1,7 @@
 package 날짜와시간;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 // Date() : 운영체제로부터 날짜와 시간 정보를 가져와 원하는 포맷 형태로 출력, 객체를 생성해야 함.
@@ -22,13 +23,41 @@ public class DateAndCal {
         f5 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         f6 = new SimpleDateFormat("오늘은 M월의 W번째 주, d번째 날 입니다.");
         f7 = new SimpleDateFormat("오늘은 yyyy년의 w주차 입니다.");
-        System.out.println(f1.format(now));
-        System.out.println(f2.format(now));
-        System.out.println(f3.format(now));
-        System.out.println(f4.format(now));
-        System.out.println(f5.format(now));
-        System.out.println(f6.format(now));
-        System.out.println(f7.format(now));
+//        System.out.println(f1.format(now));
+//        System.out.println(f2.format(now));
+//        System.out.println(f3.format(now));
+//        System.out.println(f4.format(now));
+//        System.out.println(f5.format(now));
+//        System.out.println(f6.format(now));
+//        System.out.println(f7.format(now));
+
+        // Calendar 클래스
+        Calendar cal = Calendar.getInstance(); // 운영체제로 부터 시간을 가져옴
+        System.out.println(cal.get(Calendar.YEAR));
+        System.out.println(cal.get(Calendar.MONTH) + 1); // 인덱스임을 유의 해야 함
+        System.out.println(cal.get(Calendar.DAY_OF_WEEK)); // 주에서 몇번째 날인지
+        System.out.println(cal.get(Calendar.DAY_OF_MONTH)); // 월에서 몇번째 날인지
+        System.out.println(cal.get(Calendar.AM_PM));
+        System.out.println(cal.get(Calendar.HOUR));
+        System.out.println(cal.get(Calendar.MINUTE));
+        System.out.println(cal.get(Calendar.SECOND));
+
+        // Calendar 클래스로 yyyy년MM월dd일 HH시mm분ss초 포맷과 동일하게 출력하기
+        // 내가 한 것
+        SimpleDateFormat a;
+        a = new SimpleDateFormat("yyyy년MM월dd일 HH시mm분ss초");
+        System.out.println(a.format(now));
+        // 답
+        Calendar date = Calendar.getInstance();
+        int year = date.get(Calendar.YEAR);
+        int mon = date.get(Calendar.MONTH)+1;
+        int day = date.get(Calendar.DAY_OF_MONTH);
+        int isPm = date.get(Calendar.AM_PM);
+        int hour = date.get(Calendar.HOUR);
+        int min = date.get(Calendar.MINUTE);
+        int sec = date.get(Calendar.SECOND);
+        if(isPm == 1) hour += 12;
+        System.out.printf("%d년%02d월%02d일 %02d시%02d분%02d초\n", year, mon, day,hour, min, sec);
 
     }
 }
